59c6e7fa36ab31c2782fc173024e51c61d7b30de
pellisdoamaral <72952249+pellisdoamaral@users.noreply.github.com>
2020-10-15 17:56:37 -0700

    error response for when user types in a bad command

--- commands/nope.js
+++ commands/nope.js
@@ -4,7 +4,8 @@ module.exports = {
     execute(message, args){
 
         // the user sent the bot a nonsense command
-
+        
+        // change the contents of toSend to change the error message
         toSend = `That's no way to talk to a hard-working bot.\n`;
         return message.channel.send(toSend);
 
